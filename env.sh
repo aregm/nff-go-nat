fad483c6a139691810f7a12b7702f95c52ab355b
Gregory Shimansky <gregory.shimansky@intel.com>
2018-10-02 22:17:54 +0000

    Improved protobuf compiler diagnostics and added compiled Go source

--- env.sh
+++ env.sh
@@ -2,8 +2,8 @@
 
 if [ -z "${NFF_GO}" ]
 then
-    echo "You need to define NFF_GO variable which points to root of built NFF_GO repository."
-    exit 1
+	echo "You need to define NFF_GO variable which points to root of built NFF_GO repository."
+	exit 1
 fi
 
 export RTE_TARGET=x86_64-native-linuxapp-gcc
@@ -11,3 +11,11 @@ export RTE_SDK="${NFF_GO}"/dpdk/dpdk-18.02
 export CGO_LDFLAGS_ALLOW='-Wl,--((no-)?whole-archive|((start|end)-group))'
 export CGO_CFLAGS="-I${RTE_SDK}/${RTE_TARGET}/include -O3 -std=gnu11 -m64 -pthread -march=native -mno-fsgsbase -mno-f16c -DRTE_MACHINE_CPUFLAG_SSE -DRTE_MACHINE_CPUFLAG_SSE2 -DRTE_MACHINE_CPUFLAG_SSE3 -DRTE_MACHINE_CPUFLAG_SSSE3 -DRTE_MACHINE_CPUFLAG_SSE4_1 -DRTE_MACHINE_CPUFLAG_SSE4_2 -DRTE_MACHINE_CPUFLAG_PCLMULQDQ -DRTE_MACHINE_CPUFLAG_RDRAND -DRTE_MACHINE_CPUFLAG_F16C -include rte_config.h -Wno-deprecated-declarations"
 export CGO_LDFLAGS="-L${RTE_SDK}/${RTE_TARGET}/lib -Wl,--no-as-needed -Wl,-export-dynamic"
+
+if ! command -v protoc &> /dev/null; then
+	echo You should install protobuf compiler package, e.g. \"sudo dnf install protobuf-compiler\" or \"sudo apt-get install protobuf-compiler\"
+fi
+if ! command -v protoc-gen-go &> /dev/null; then
+	echo You should install Go plugin for protobuf compiler with \"go get github.com/golang/protobuf/protoc-gen-go\" and add target directory to PATH \(\$GOPATH/bin or \~/go/bin\)
+fi
+
---
+++ updatecfg/compile-proto.sh
@@ -0,0 +1,1 @@
+protoc -I . --go_out=plugins=grpc:. updatecfg.proto
